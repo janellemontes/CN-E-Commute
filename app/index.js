8f6be6a74ab0a207aabd369474cd8b0c0a81619f
janellemontes <118874283+janellemontes@users.noreply.github.com>
2024-12-07 16:16:35 +0800

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>CN E-Commute</Text>
+        </View>
+    )
+}
+
+export default Home;
